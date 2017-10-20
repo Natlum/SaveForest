f1ad3f0424d0e62aa456b5e595d5f5a9a501c08f
José Juan Sosa Rodríguez <25982334+Natlum@users.noreply.github.com>
2017-10-20 18:46:33 +0100

    Anadido MapView en el fragmento 2 de la actividad step A

--- app/src/main/java/es/jjsr/saveforest/fragments/StepA/Step2Fragment.java
+++ app/src/main/java/es/jjsr/saveforest/fragments/StepA/Step2Fragment.java
@@ -6,14 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import es.jjsr.saveforest.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Step2Fragment extends Fragment {
+public class Step2Fragment extends Fragment implements OnMapReadyCallback{
 
+
+    private Spinner spinner;
+    private final int ESPANA = 2;
+    private GoogleMap map;
 
     public Step2Fragment() {
         // Required empty public constructor
@@ -32,7 +46,39 @@ public class Step2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step2, container, false);
+        View v = inflater.inflate(R.layout.fragment_step2, container, false);
+        spinnerStart(v);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment != null){
+            mapFragment.getMapAsync(this);
+        }
+
+        return v;
+
     }
 
+    private void spinnerStart(View v){
+        spinner = v.findViewById(R.id.SpinerCountry);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.countries_array_name, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(ESPANA);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), "Se ha elegido el: " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+    }
 }
