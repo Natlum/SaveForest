61aa8151430440d1ca2bf1f0e8d4fac57fd21bbe
José Juan Sosa Rodríguez <25982334+Natlum@users.noreply.github.com>
2017-12-24 13:46:57 +0000

    Se ha cambiado la librería por la VolleyPlus, pero sigue sin funcionar bien

--- app/src/main/java/es/jjsr/saveforest/volley/AdviceVolley.java
+++ app/src/main/java/es/jjsr/saveforest/volley/AdviceVolley.java
@@ -4,10 +4,14 @@ import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.JsonArrayRequest;
+import com.android.volley.request.JsonObjectRequest;
+import com.android.volley.request.StringRequest;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonArrayRequest;
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
