221dfdc134f510058c7709fd8d3cf27d8d61bbe5
José Juan Sosa Rodríguez <25982334+Natlum@users.noreply.github.com>
2018-02-12 19:45:21 +0000

    Se ha modificado las preferencias para que tenga un estilo. Y el contenido

--- app/src/main/java/es/jjsr/saveforest/resource/SettingActivity.java
+++ app/src/main/java/es/jjsr/saveforest/resource/SettingActivity.java
@@ -3,6 +3,10 @@ package es.jjsr.saveforest.resource;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import es.jjsr.saveforest.R;
 
@@ -12,5 +16,15 @@ public class SettingActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar toolbar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(toolbar, 0);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
